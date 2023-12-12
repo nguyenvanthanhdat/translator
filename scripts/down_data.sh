33f04ac2f43490952bbb3c280d9afca28e3453e6
datnvt <nguyenvanthanhdat1810@gmail.com>
2023-12-12 18:49:46 +0700

    Fix: Change save file path to correct

--- scripts/down_data.sh
+++ scripts/down_data.sh
@@ -5,7 +5,7 @@ gdown 1prTTLZX6AbGP2n3Epm0y4ujDBx7UNj13
 mkdir data
 # python -m zipfile -e PhoMT.zip data/craw
 # python -m zipfile -e VietAI.zip data/craw/VietAI
-python -m  zipfile -e dataset_1000.zip data/craw/finetune
+python -m  zipfile -e dataset_1000.zip data/craw
 
 # rm PhoMT.zip
 # rm VietAI.zip
