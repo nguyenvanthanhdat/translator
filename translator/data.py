bcf455ab434516cb8e47c8df61f1f73924063212
datnvt <nguyenvanthanhdat1810@gmail.com>
2023-12-19 11:19:10 +0700

    Fix: Dataset after take function

--- translator/data.py
+++ translator/data.py
@@ -34,7 +34,8 @@ class Processor:
                 streaming = self.data_args.streaming
             )
             if self.data_args.max_train_samples is not None:
-                train_data = train_data.take(range(self.data_args.max_train_samples))
+                train_data = list(train_data.take(range(self.data_args.max_train_samples)))
+                train_data = Dataset.from_list(train_data)
             dataset['train'] = self.process_fn(train_data)
             
         # validation set
@@ -53,7 +54,8 @@ class Processor:
                 streaming = self.data_args.streaming
             )
             if self.data_args.max_valid_samples is not None:
-                valid_data = valid_data.take(range(self.data_args.max_valid_samples))
+                valid_data = list(valid_data.take(range(self.data_args.max_valid_samples)))
+                valid_data = Dataset.from_list(valid_data)
             dataset['validation'] = self.process_fn(valid_data)
         
         return dataset
