168cfb15f6cd70ba2135c9360022c1fc1c46d416
datnvt <nguyenvanthanhdat1810@gmail.com>
2023-12-23 23:49:27 +0700

    Fix: KeyError 'target'

--- translator/eval.py
+++ translator/eval.py
@@ -46,7 +46,7 @@ def get_output(examples, model, tokenizer, max_length, num_beams):
     # inptus = dict(inputs)
     inputs.pop('len')
     inputs.pop('input')
-    inputs.pop('target')
+    inputs.pop('label')
     inputs = {key: torch.tensor(inputs[key]).to('cuda') for key in inputs}
     outputs = model.generate(**inputs, 
                             #  max_new_tokens=max_length,
