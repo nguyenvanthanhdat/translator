5fe5930fe5e07293e5e69d0933d618ba6deb08ad
datnvt <nguyenvanthanhdat1810@gmail.com>
2023-12-27 16:11:04 +0700

    Feature: Retrain mt5 with warmup_ratio 0.1

--- scripts/train_translate.sh
+++ scripts/train_translate.sh
@@ -1,13 +1,14 @@
 MODEL_NAME_OR_PATH="google/mt5-large"
 # MODEL_NAME_OR_PATH="model/pt_full_model"
 HF_TOKEN='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' 
-DATASET_NAME_TRAIN ='presencesw/hash_20_256_v2'
+DATASET_NAME_TRAIN='presencesw/hash_v3'
 DATASET_NAME_VALIDATION='presencesw/phomt_eval'
 OUTPUT_DIR='outputs'
 SAVE_DIR='model'
-BZ=2
+BZ=3
 ALL_STEP=50_000_000
-SAVE_EVAL_STEP=2_000_000
+SAVE_EVAL_STEP=5_000
+GRA_ACC=14
 
 
 !WANDB_PROJECT=translator WANDB_API_KEY=138c38699b36fb0223ca0f94cde30c6d531895ca CUDA_VISIBLE_DEVICES=0,1 accelerate launch --gpu_ids all --num_processes 2 \
@@ -21,19 +22,16 @@ SAVE_EVAL_STEP=2_000_000
     --dataset_name_validation $DATASET_NAME_VALIDATION \
     --max_steps $ALL_STEP \
     --streaming \
-    --use_lora \
-    --linear_layer \
-    --att_blocks \
+    --warmup_ratio 0.1 \
     --per_device_train_batch_size $BZ \
     --per_device_eval_batch_size $BZ \
     --num_train_epochs 4 \
-    --gradient_accumulation_steps 1\
-    --max_len 256 \
+    --gradient_accumulation_steps $GRA_ACC \
+    --max_len 512 \
     --save_steps $SAVE_EVAL_STEP \
     --eval_steps $SAVE_EVAL_STEP \
     --logging_steps 1000 \
-    --save_total_limit 1 \
+    --save_total_limit 3 \
     --evaluation_strategy 'steps' \
-    --att_blocks \
     --overwrite_output_dir \
     --load_best_model_at_end
