de87e91160b700236a75b05fb69981a8b7facac8
datnvt <nguyenvanthanhdat1810@gmail.com>
2024-01-09 23:30:54 +0700

    Feature: Eval with lora

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,19 +1,20 @@
-# MODEL_NAME_OR_PATH="checkpoint-*"
-MODEL_NAME_OR_PATH="google/mt5-small"
+MODEL_NAME_OR_PATH="google/mt5-large"
 # MODEL_NAME_OR_PATH="model/pt_full_model"
-TOKEN_NAME='google/mt5-small'
+TOKEN_NAME='google/mt5-large'
 HF_TOKEN='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' 
 DATA_TEST_PATH='presencesw/phomt_eval'
 SPLIT='test'
 
 
-CUDA_VISIBLE_DEVICES=0,1 python -m translator.eval \
+!CUDA_VISIBLE_DEVICES=0,1 accelerate launch --gpu_ids 0,1 --num_processes=2 -m translator.eval \
     --model_name_or_path $MODEL_NAME_OR_PATH \
     --tokenizer_name_or_path $TOKEN_NAME \
     --hf_key $HF_TOKEN \
     --data_test_path $DATA_TEST_PATH \
     --split $SPLIT \
-    --num_proc 1 \
-    --max_len 256 \
+    --num_proc 2 \
+    --batch_size 1 \
+    --max_len 512 \
     --num_beams 3,4,5 \
     --hf_key $HF_TOKEN \
+    --use_lora True 
