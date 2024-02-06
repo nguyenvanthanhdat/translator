255f64cfb31c6b91c0cdacba3bf557473496a65e
datnvt <nguyenvanthanhdat1810@gmail.com>
2024-02-06 20:18:50 +0700

    fix: turn off paged_adamw_8bit optim

--- translator/training.py
+++ translator/training.py
@@ -196,7 +196,7 @@ def main():
             base_model = prepare_model_for_kbit_training(base_model)
             base_model.gradient_checkpointing_enable()  # reduce number of stored activations
             base_model.enable_input_require_grads()
-            training_args.optim = "paged_adamw_8bit"
+            # training_args.optim = "paged_adamw_8bit"
             training_args.fp16=True
 
         # add LoRA adaptor
