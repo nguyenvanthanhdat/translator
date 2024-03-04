3e5ff2f62a26b7b80f02c1111fa2ffb660298d5e
datnvt <nguyenvanthanhdat1810@gmail.com>
2024-03-04 13:39:39 +0700

    Feat: Update script down lora 11K vs 11.5K v1.2

---
+++ scripts/down_lora_11K.sh
@@ -0,0 +1,3 @@
+mkdir lora
+cd lora
+gdown https://drive.google.com/drive/folders/1XbnAqCxLYZEFeLTyVtZZj4GepCf30VoN?usp=sharing --folder
---
+++ scripts/down_lora_11_5K.sh
@@ -0,0 +1,3 @@
+mkdir lora
+cd lora
+gdown https://drive.google.com/drive/folders/1nI5et56VL9R16DAt-kGoirAflTWEWODt?usp=sharing --folder
