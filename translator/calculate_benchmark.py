1fe5a7d49de3f6bffab7b8882a7ffb1fdd5e6ce4
datnvt <nguyenvanthanhdat1810@gmail.com>
2024-03-11 15:45:47 +0700

    Fix: load and save path

--- translator/calculate_benchmark.py
+++ translator/calculate_benchmark.py
@@ -2,8 +2,9 @@ import os, evaluate, sys
 from datasets import load_dataset
 
 def main(lora_path):
+    path = os.getcwd()
     os.chdir(f"eval-{lora_path}")
-    os.getcwd()
+    
     bleu = evaluate.load("bleu")
     score = open("score.txt", "w")
     for file_txt in os.listdir("."):
@@ -15,10 +16,10 @@ def main(lora_path):
             results = bleu.compute(predictions=dataset['predict'], references=dataset['label'])
         except:
             results = 0
-    score.write(f"{file_txt}: bleu - {results}\n")
+        score.write(f"{file_txt}: bleu - {results}\n")
     score.close()
-    os.chdir("..")
-    os.system("zip -r result.zip eval")
+    os.chdir(path)
+    #os.system(f"zip -r result.zip eval-{lora_path}")
 
 if __name__ == "__main__":
     lora_path = sys.argv[1]
